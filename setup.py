e6c86a15b8d20b502287ad0588bbf33d381bbe57
dante-biase <dante.biase@outlook.com>
2021-12-09 21:57:36 -0500

    updated project files

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'pinput',
   packages = ['pinput'],
-  version = '1.0',
+  version = '1.0.1',
   license='MIT',
   description = '',
   author = 'Dante Biase',
-  url = 'https://github.com/dante-biase',
-  download_url = 'https://github.com/dante-biase/pinput/archive/1.0.tar.gz',
+  url = 'https://github.com/dante-biase/pinput',
+  download_url = 'https://github.com/dante-biase/pinput/archive/v1.0.1.tar.gz',
   classifiers=[
     'Development Status :: 5 - Production/Stable',
     'Intended Audience :: Developers',
